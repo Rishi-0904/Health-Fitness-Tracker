2a767a35ab259e842fce3c5f905d371e5d8da500
Rishi_0904 <rishi.20243237@mnnit.ac.in>
2025-10-14 20:36:56 +0530

    enhancing css to pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import { AuthProvider } from "./state/AuthContext.jsx";
-import "./styles.css";
+import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
